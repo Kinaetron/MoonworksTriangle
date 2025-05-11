2b11f96cfa0557abe34ae37cc97b07ea1e55fe94
Darren <darren.daley@hotmail.co.uk>
2025-05-11 09:39:51 +0100

    Initial Example Setup

---
+++ MoonworksTriangle/BasicTriangle/BasicTriangleGame.cs
@@ -0,0 +1,38 @@
+﻿using MoonWorks;
+using MoonWorks.Graphics;
+
+namespace BasicTriangle;
+
+public class BasicTriangleGame : Game
+{
+
+    public BasicTriangleGame(
+        AppInfo appInfo,
+        WindowCreateInfo windowCreateInfo,
+        FramePacingSettings framePacingSettings
+        ) : base(
+            appInfo,
+            windowCreateInfo,
+            framePacingSettings,
+            ShaderFormat.SPIRV | ShaderFormat.DXIL | ShaderFormat.MSL | ShaderFormat.DXBC)
+    {
+    }
+
+    protected override void Update(TimeSpan delta)
+    {
+    }
+
+    protected override void Draw(double alpha)
+    {
+        CommandBuffer cmdbuf = GraphicsDevice.AcquireCommandBuffer();
+        Texture swapchainTexture = cmdbuf.AcquireSwapchainTexture(MainWindow);
+        if (swapchainTexture != null)
+        {
+            var renderPass = cmdbuf.BeginRenderPass(
+                new ColorTargetInfo(swapchainTexture, Color.Black)
+            );
+            cmdbuf.EndRenderPass(renderPass);
+        }
+        GraphicsDevice.Submit(cmdbuf);
+    }
+}
---
+++ MoonworksTriangle/BasicTriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using BasicTriangle;
+using MoonWorks;
+
+internal class Program
+{
+    static void Main()
+    {
+        var windowCreateInfo = new WindowCreateInfo(
+             "Triangle Example Game",
+             1280,
+             720,
+             ScreenMode.Windowed
+         );
+
+        var framePacingSettings = FramePacingSettings.CreateLatencyOptimized(60);
+
+        var game = new BasicTriangleGame(
+            new AppInfo("Triangle Example Game", "TriangleExampleGame"),
+            windowCreateInfo,
+            framePacingSettings);
+
+        game.Run();
+    }
+}
